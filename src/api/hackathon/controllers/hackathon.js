f946408e23eb60ad8138290152ddda74b0455dc5
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-12-01 01:40:12 +0530

    feature: events/hackathon collection type created in scheme 👀

---
+++ src/api/hackathon/controllers/hackathon.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hackathon controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hackathon.hackathon');
---
+++ src/api/hackathon/routes/hackathon.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hackathon router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hackathon.hackathon');
